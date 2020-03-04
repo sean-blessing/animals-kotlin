705b4ce553e3ee288e00c91f0b66675ae2e84b5a
Sean Blessing <snblessing@gmail.com>
2020-03-03 21:27:16 -0500

    init commit

---
+++ src/Animals.kt
@@ -0,0 +1,23 @@
+class Animal {
+    val image = ""
+    val food = ""
+    val habitat = ""
+    var hunger = 10
+
+    fun makeNoise() {
+        println("The Animal is making a noise")
+    }
+
+    fun eat() {
+        println("The Animal is eating")
+    }
+
+    fun roam() {
+        println("The Animal is roaming")
+    }
+
+    fun sleep() {
+        println("The Animal is sleeping")
+    }
+
+}
